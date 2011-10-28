cadef1ec52e86362272a8bea0144eda2fc8d203d
lapsamal <lapsamal@4GBD02934.NOE.Nokia.com>
2011-10-28 15:07:45 +0100

    performance tests

--- tests/amqp/app.py
+++ tests/amqp/app.py
@@ -6,6 +6,7 @@ __version__ = '0.0.1'
 from nucleon.framework import Application
 import logging
 import gevent
+from gevent.queue import Queue
 log = logging.getLogger(__name__)
 app = Application()
 
@@ -33,6 +34,43 @@ def version(request):
     """An example view that returns the version of the app."""
     return {'version': __version__}
 
+@app.view('/push')
+def push(request):
+    """
+    view for performance tests
+    """
+    sent_results = Queue()
+    recv_results = Queue()
+
+    def sent_callback(promise,result):
+        sent_results.put(result)
+        log.debug("Received result %s" % result)
+
+    def recv_callback(promise,result):
+        recv_results.put(result)
+        log.debug("Received result %s" % result)
+
+    with app.get_amqp_pool().connection() as conn:
+        with app.get_amqp_pool(type="listen").connection() as connR:
+            conn.basic_async_publish(exchange="unit_test_room",routing_key="user1",body="Ping Message",callback=sent_callback)
+            connR.basic_async_get_and_ack(queue="listener1",callback=recv_callback)
+
+            recv = recv_results.get()
+            sent = sent_results.get()
+
+            return {'value': recv['body']}
+
+@app.view('/push_sync')
+def push_sync(request):
+    """
+    view for performance tests
+    """
+    with app.get_amqp_pool().connection() as conn:
+        with app.get_amqp_pool(type="listen").connection() as connR:
+            sent = conn.basic_sync_publish(exchange="unit_test_room",routing_key="user1",body="Ping Message")
+            recv = connR.basic_sync_get_and_ack(queue="listener1")
+
+            return {'value': recv['body']}
 
 @app.on_start
 def configure_amqp():
