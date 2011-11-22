c5d4efeed11194381773f5842e4e07887b907421
Daniel Pope <ext-dan.pope@nokia.com>
2011-11-22 10:44:19 +0000

    Fix: a couple of tweaks to the fabfile for building docs

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,6 @@ from fabric.contrib.project import rsync_project
 
 @hosts('bruges.vertulabs.co.uk')
 def deploy_nucleondocs():
-
     """Generate nucleon docs and deploy to the docs webserver"""
 
     PATH = '/srv/docs_webserver/docs/nucleon/'
@@ -19,14 +18,12 @@ def deploy_nucleondocs():
 
     # Copy generated docs to docs_webserver on target machine
     sudo('chown -R %s: %s' % (env.user, PATH))
-    rsync_project(remote_dir=PATH, local_dir=join(dirname(__file__),'docs/_build/html'))
+    rsync_project(remote_dir=PATH, local_dir=join(dirname(__file__), 'docs/_build/html/'), delete=True)
     sudo('chown -R docs:www-data ' + PATH)
 
 
 @runs_once
 def build_nucleondocs():
-
     # Generate nucleon docs (uses Sphinx)
-    with cd(join(dirname(__file__),'docs')):
-        local('make html')
-
+    path = join(dirname(__file__),'docs')
+    local('cd %s && make html' % path)
