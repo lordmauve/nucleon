92a6a6fe85a9c0d1e383c00fe9d49d6eafbbe60b
Daniel Pope <ext-dan.pope@nokia.com>
2012-03-05 09:56:01 +0000

    Fix: specify dependency_links to give a download location for gevent-1.0b1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 INSTALL_REQUIRES = [
         'WebOb>=1.1.1',
-        'gevent==1.0b1',
+        'gevent==1.0b1',  # will be installed with dependency_links
         'psycopg2>=2.2.1',
         'WebTest>=1.3.1',
         'puka>=0.0.3',
@@ -30,4 +30,7 @@ setup(
         'nucleon': ['skel/*'],
     },
     install_requires=INSTALL_REQUIRES,
+    dependency_links=[
+        'http://code.google.com/p/gevent/downloads/list',
+    ]
 )
