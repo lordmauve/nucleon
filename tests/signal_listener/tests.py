d630fafe865573f8c06e1fdde98c5273f466fa6d
Daniel Pope <ext-dan.pope@nokia.com>
2012-03-05 16:14:45 +0000

    Fix: start signal_listener process on an unused port

--- tests/signal_listener/tests.py
+++ tests/signal_listener/tests.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import os
 import signal
@@ -21,6 +22,31 @@ processes = []
 # $ nose:nosetests -v -s
 
 
+def listening_ports():
+    """Determine what ports are in use by reading Linux /proc filesystem."""
+    import re
+    listening = set()
+    with open('/proc/net/tcp', 'r') as f:
+        for l in f:
+            mo = re.match(r'\s*\d+: [0-9A-F]{8}:([0-9A-Z]{4}) 0{8}:0{4}', l)
+            if mo:
+                listening.add(int(mo.group(1), 16))
+    return listening
+
+
+def get_free_port():
+    """Search for a free TCP port between 7000 and 10000."""
+    listening = listening_ports()
+    for i in xrange(7000, 10000):
+        if i not in listening:
+            return i
+    raise IOError("No port available to bind")
+
+
+SERVER_PORT = get_free_port()
+BASE_URL = 'http://localhost:%d/' % SERVER_PORT
+
+
 class SubNucleon(object):
     """
     Abstraction over Nucleon http/amqp server to start, kill, join it.
@@ -33,7 +59,9 @@ class SubNucleon(object):
     - multiprocessing.Process just works
     """
     def __init__(self):
-        self.sp = Process(target=nucleon.commands.start)
+        args = '--port', str(SERVER_PORT)
+        self.sp = Process(target=nucleon.commands.start, args=args)
+        print >>sys.stderr, "Starting nucleon on port", SERVER_PORT
         self.sp.start()
 
     def sigusr1(self):
@@ -60,7 +88,7 @@ def get_page():
     verifies that page return takes 3s (hardcoded in app.py)
     """
     t_start = time.time()
-    f = urllib2.urlopen('http://localhost:8888/')
+    f = urllib2.urlopen(BASE_URL)
     resp = f.read()
     f.close()
     eq_(json.loads(resp), {'version': '0.0.1'})
@@ -79,7 +107,7 @@ def get_page_503():
     t_start = time.time()
     code = 200
     try:
-        f = urllib2.urlopen('http://localhost:8888/')
+        f = urllib2.urlopen(BASE_URL)
         f.read()
         f.close()
     except urllib2.HTTPError as e:
